1c1fbae079fccaefbd91f26208d8fdfc5130d413
Xavier Folch <Xavier Folch>
2017-01-21 18:03:02 +0100

    initial commit - coders strike back - gold league

---
+++ coders_strike_back/gold_league_optimizer.py
@@ -0,0 +1,161 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+class Pod:
+    
+    def __init__(self, name):
+        self.x, self.y, self.vx, self.vy, self.angle, self.nextCheckPointId, self.thrust = 0,0,0,0,0,0,0
+        self.next_checkpoint_x, self.next_checkpoint_y = 0, 0
+        self.next_checkpoint_angle, self.next_checkpoint_dist = 0, 0
+        self.name = name
+        
+    def configure(self,input, list_checkpoints):
+        self.x, self.y, self.vx, self.vy, self.angle, self.nextCheckPointId = [int(i) for i in input().split()]
+        self.next_checkpoint_x, self.next_checkpoint_y = list_checkpoints[self.nextCheckPointId]        
+        self.next_checkpoint_angle = self.compute_angle_next_checkpoint() 
+        self.next_checkpoint_dist = math.sqrt((self.next_checkpoint_x - self.x)**2 + (self.next_checkpoint_y - self.y)**2)
+        
+    def compute_angle_next_checkpoint(self) :
+        #Compute the angle between the pod direction and the next checkpoint
+        #function of the current angle of the pod
+        #based of the scalar product of two vectors : cos Angle = dotProduct(v1, v2) / (norm(v1) * norm(v2))
+        #To set the pod's orientation vector, use the angle between the x game axis and the pod direction (self.angle)
+        #Scale the cos/sin angle by 1000 to be coherent with the game scale (16 0000 x 9 0000)
+        
+        #Set the normalized vector between the pod and the next checkpoint
+        vect_pod_checkpoint_x = self.next_checkpoint_x - self.x
+        vect_pod_checkpoint_y = self.next_checkpoint_y - self.y
+        norm_vect_pod_checkpoint = math.sqrt(vect_pod_checkpoint_x**2 + vect_pod_checkpoint_y**2)
+        vect_pod_checkpoint_x = vect_pod_checkpoint_x / norm_vect_pod_checkpoint
+        vect_pod_checkpoint_y = vect_pod_checkpoint_y / norm_vect_pod_checkpoint
+        
+        #Set the normalized vector of the pod's current direction
+        vect_pod_direction_x = 1000 * math.cos(math.radians(self.angle))
+        vect_pod_direction_y = 1000 * math.sin(math.radians(self.angle))
+        norm_vect_pod_direction = math.sqrt(vect_pod_direction_x**2 + vect_pod_direction_y**2)
+        vect_pod_direction_x = vect_pod_direction_x / norm_vect_pod_direction
+        vect_pod_direction_y = vect_pod_direction_y / norm_vect_pod_direction
+        
+        #use round to avoid float imprecision
+        scalar_product = round((vect_pod_checkpoint_x * vect_pod_direction_x) + (vect_pod_checkpoint_y * vect_pod_direction_y),8)
+        
+        print(self.name + " scalar : " + str(scalar_product), file=sys.stderr)
+        print(self.name + " angle : " + str( math.degrees(math.acos(scalar_product))), file=sys.stderr)
+        
+        #angle = acos(dotProduct(v1, v2) / (norm(v1) * norm(v2))) and normalized vector has a norm of 1
+        return math.degrees(math.acos(scalar_product))        
+        
+def optimize_thrust(player_x, player_y, boss_x, boss_y, checkpoint_x, checkpoint_y, checkpoint_angle, checkpoint_dist, is_shield_available) :
+    
+    SAFETY_DISTANCE = 830
+    BOOST_DISTANCE = 4200
+    X_DELTA_POSITION = 600
+    Y_DELTA_POSITION = 600
+    MIN_THRUST = 10
+    MAX_THRUST = 100
+    BOOST = 'BOOST'
+    SHIELD = 'SHIELD'
+    MIN_TO_CHECKPOINT = 100
+    MAX_ANGLE = 120
+    MIN_ANGLE = 20
+    SLOWING_COEFF = 0.4
+    TURN_SHIELD_OFF = 4
+    FIRST_SLOWING_DOWN_COEFF_DIST = 8
+    FIRST_SLOWING_COEFF = 0.9
+    SECOND_SLOWING_DOWN_COEFF_DIST = 4
+    SECOND_SLOWING_COEFF = 0.2
+    SHIELD_ACTIVATION_COEFF_DIST = 7
+        
+    distance_player_ennemy = math.sqrt((player_x - boss_x)**2 + (player_y - boss_y)**2)
+        
+    new_destination_x = checkpoint_x
+    new_destination_y = checkpoint_y
+    
+    #if distance_player_ennemy < SAFETY_DISTANCE :
+    if boss_x < player_x and player_x < checkpoint_x : 
+        new_destination_x = new_destination_x - X_DELTA_POSITION
+    elif boss_x > player_x and player_x > checkpoint_x :     
+        new_destination_x = new_destination_x + X_DELTA_POSITION
+        
+    if boss_y < player_y and player_y < checkpoint_y : 
+        new_destination_y = new_destination_y - Y_DELTA_POSITION
+    elif boss_y > player_y and player_y > checkpoint_y :     
+        new_destination_y = new_destination_y + Y_DELTA_POSITION        
+                    
+    abs_angle = abs(checkpoint_angle)   
+    
+    if abs_angle > MAX_ANGLE :
+        thrust = MIN_THRUST      
+    elif abs_angle < MIN_ANGLE :
+        thrust = MAX_THRUST
+    else :
+        thrust = 100 * ((MAX_ANGLE - abs_angle) / MAX_ANGLE)
+    
+    coefficient = 1    
+    if checkpoint_dist < MIN_TO_CHECKPOINT :
+        #coefficient = 1-((MIN_TO_CHECKPOINT - next_checkpoint_dist) / MIN_TO_CHECKPOINT)
+        coefficient = 0
+    elif checkpoint_dist < SECOND_SLOWING_DOWN_COEFF_DIST * MIN_TO_CHECKPOINT :
+        coefficient = SECOND_SLOWING_COEFF
+    elif checkpoint_dist < FIRST_SLOWING_DOWN_COEFF_DIST * MIN_TO_CHECKPOINT :
+        coefficient = FIRST_SLOWING_COEFF
+    
+    if (thrust < MIN_THRUST) : 
+            thrust = MIN_THRUST
+            
+    thrust = math.ceil(thrust * coefficient)
+        
+    if checkpoint_dist > BOOST_DISTANCE and checkpoint_angle < MIN_ANGLE :
+        thrust = BOOST
+        
+    #if checkpoint_dist < SHIELD_ACTIVATION_COEFF_DIST * MIN_TO_CHECKPOINT and distance_player_ennemy <= SAFETY_DISTANCE and is_shield_available :
+    if (coefficient < 1 or checkpoint_dist < SHIELD_ACTIVATION_COEFF_DIST * MIN_TO_CHECKPOINT) and distance_player_ennemy <= SAFETY_DISTANCE and is_shield_available :
+        thrust = SHIELD
+        time_before_availability_shield = TURN_SHIELD_OFF
+        
+    return [new_destination_x, new_destination_y, thrust]
+
+# game loop
+
+# Initialization
+laps = int(input())
+checkpointCount = int(input())
+
+list_checkpoints = []
+for i in range(checkpointCount) :
+    list_checkpoints.append([int(i) for i in input().split()])
+
+is_boost_used = False
+time_before_availability_shield = 0 #TODO : one shield by pod
+
+cho = Pod("cho")
+gall = Pod("gall")
+boss1 = Pod ("boss1")
+boss2 = Pod ("boss2")
+
+while True:
+    
+    cho.configure(input, list_checkpoints)
+    gall.configure(input, list_checkpoints)
+    boss1.configure(input, list_checkpoints)
+    boss2.configure(input, list_checkpoints)
+    
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+   
+    cho_optimized_data = optimize_thrust(cho.x, cho.y, boss1.x, boss1.y, cho.next_checkpoint_x, cho.next_checkpoint_y, cho.next_checkpoint_angle, cho.next_checkpoint_dist, time_before_availability_shield <= 0)
+    gall_optimized_data = optimize_thrust(gall.x, gall.y, boss1.x, boss1.y, gall.next_checkpoint_x, gall.next_checkpoint_y, gall.next_checkpoint_angle, gall.next_checkpoint_dist, time_before_availability_shield <= 0)
+
+    print(cho.name + " thrust : " + str( cho_optimized_data[2]), file=sys.stderr)
+    print(gall.name + " thrust : " + str( gall_optimized_data[2]), file=sys.stderr)
+
+    #time_before_availability_shield = time_before_availability_shield - 1
+    
+    # You have to output the target position
+    # followed by the power (0 <= thrust <= 100)
+    # i.e.: "x y thrust"
+    print(str(cho_optimized_data[0]) + " " + str(cho_optimized_data[1]) + " " + str(cho_optimized_data[2]))
+    print(str(gall_optimized_data[0]) + " " + str(gall_optimized_data[1]) + " " + str(gall_optimized_data[2]))
